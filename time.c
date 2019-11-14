048faf75a0b321786a51b3f6a2c2f3fba791ac15
swayinwind <liyuanquan@qiniu.com>
2019-11-14 18:20:43 +0800

    Update time.c

--- time.c
+++ time.c
@@ -5,3 +5,6 @@ int64_t gettime_ms()
 
     return ((int64_t)(tp.tv_sec * 1000000000ll + tp.tv_nsec))/1000000;
 }
+
+#define DBG_TIME_START()  { int64_t start = 0,end = 0;start = gettime_ms();
+#define DBG_TIME_COST()   end = gettime_ms();LOGI("cost:%"PRId64"", end-start); }

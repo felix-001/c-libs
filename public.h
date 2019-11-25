0911ee24aaccc1ef561fb59e4e95f77a0c7d5ad5
swayinwind <liyuanquan@qiniu.com>
2019-11-25 17:47:11 +0800

    Create public.h

---
+++ public.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <fcntl.h>
+#include <string.h>
+#include <time.h>
+#include <sys/time.h>
+#include <pthread.h>
+#include <inttypes.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <assert.h>

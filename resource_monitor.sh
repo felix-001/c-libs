8da074c34a1e023e1a4c8cc6d7c2039fedb5cae1
swayinwind <liyuanquan@qiniu.com>
2019-12-16 17:08:15 +0800

    Update resource_monitor.sh

--- resource_monitor.sh
+++ resource_monitor.sh
@@ -1,16 +1,22 @@
 #!/bin/sh
 
-pid=$(ps | grep $1 | grep -v grep | grep -v monitor.sh| awk '{print $1}')
+#output=/mnt/mmc0/monitor.log
+output=/dev/tty
 while true
 do
+    pid=$(ps | grep $1 | grep -v grep | grep -v monitor.sh| grep -v gdb| awk '{print $1}')
+    if [ ! -n "$pid" ];then
+    	echo "process exit, exit monitor"
+    	exit 1
+    fi
     time=$(date "+%Y-%m-%d %H:%M:%S")
-    echo $time >> monitor.log
-    echo =================== >> monitor.log
-    cat /proc/$pid/status | grep VmRSS >> monitor.log
-    free >> monitor.log
+    mem=$(cat /proc/$pid/status | grep VmRSS | awk {'print $2'})
+    echo "["$time"] mem: " $mem>> $output
+    system_free_mem=$(free | sed -n '2p' | awk '{print $4}')
+    echo "["$time"]" system free mem: $system_free_mem >> $output
     fd_count=$(ls /proc/$pid/fd | wc -l)
-    echo "fd: "$fd_count >> monitor.log
-    echo "  PID  PPID USER     STAT   VSZ %VSZ CPU %CPU COMMAND" >> monitor.log
-    top -n 1 | grep $1 | grep -v grep >> monitor.log
+    echo "["$time"] fd: " $fd_count >> $output
+    cpu_usage=$(top -n 1 | grep $1 | grep -v grep | grep -v gdb | awk '{print $9}')
+    echo "["$time"] cpu_usage: " $cpu_usage >> $output
     sleep 3
 done
